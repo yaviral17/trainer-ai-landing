f2b1dbdf4a00cfd1b300f43e7633911bcdba6955
aviral <yaviral17@gmail.com>
2025-05-18 19:55:43 +0530

    Add Vercel Analytics for improved user tracking
    - Included the @vercel/analytics package in package.json.
    - Integrated the Analytics component in the RootLayout of layout.tsx for tracking user interactions.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import Header from '@/components/Header';
 import { ThemeProvider } from '@/context/ThemeContext';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 export default function RootLayout({
   children,
@@ -16,6 +17,7 @@ export default function RootLayout({
           <Header activeTab="" onTabChange={() => {}} />
           {children}
         </ThemeProvider>
+         <Analytics />
       </body>
     </html>
   );
